d84993c48f2480ed832959898a98b43ff7bfacd2
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-05-19 14:23:57 -0500

    font and colors

--- app/src/main/java/com/navi/musicplayerapp/ui/theme/Color.kt
+++ app/src/main/java/com/navi/musicplayerapp/ui/theme/Color.kt
@@ -2,10 +2,6 @@ package com.navi.musicplayerapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple80 = Color(0xFF2D2E37)
+val PurpleGrey80 = Color(0xFFC4C4C4)
+val Pink80 = Color(0xFF3C3ABE)
